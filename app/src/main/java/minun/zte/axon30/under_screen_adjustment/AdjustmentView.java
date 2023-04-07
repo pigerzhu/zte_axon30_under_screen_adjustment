0d53434fcfd74754289cdc286f2e92df6b2193f0
pigerzhu <64568939+pigerzhu@users.noreply.github.com>
2023-04-08 00:01:05 +0800

    Update AdjustmentView.java

--- app/src/main/java/minun/zte/axon30/under_screen_adjustment/AdjustmentView.java
+++ app/src/main/java/minun/zte/axon30/under_screen_adjustment/AdjustmentView.java
@@ -24,7 +24,7 @@ public class AdjustmentView extends View {
         setOutlineProvider(new ViewOutlineProvider() {
             @Override
             public void getOutline(View view, Outline outline) {
-                Rect rect = new Rect(10, 10, 94, 94);
+                Rect rect = new Rect(11, 11, 93, 93);
                 Log.d("AdjustmentView", "rect: "+ rect);
                 outline.setRoundRect(rect, 45);
             }
