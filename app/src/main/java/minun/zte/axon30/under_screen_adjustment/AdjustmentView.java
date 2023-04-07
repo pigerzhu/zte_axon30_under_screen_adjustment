a6b6ba4db3ff68263fa394fe6e7002638ea00ff2
pigerzhu <64568939+pigerzhu@users.noreply.github.com>
2023-04-07 19:59:51 +0800

    Update AdjustmentView.java

--- app/src/main/java/minun/zte/axon30/under_screen_adjustment/AdjustmentView.java
+++ app/src/main/java/minun/zte/axon30/under_screen_adjustment/AdjustmentView.java
@@ -14,45 +14,45 @@ import android.view.ViewOutlineProvider;
 
 public class AdjustmentView extends View {
 
-    Paint paint;
+//    Paint paint;
 
     public AdjustmentView(Context context) {
         super(context);
         this.setBackgroundColor(Color.TRANSPARENT);
-        this.paint = new Paint();
-        this.paint.setAlpha(100);
+//        this.paint = new Paint();
+//        this.paint.setAlpha(100);
         setOutlineProvider(new ViewOutlineProvider() {
             @Override
             public void getOutline(View view, Outline outline) {
                 Rect rect = new Rect(7, 7, 90+7, 90+7);
                 Log.d("AdjustmentView", "rect: "+ rect);
-                outline.setRoundRect(rect, 45);
+                outline.setRoundRect(rect, 5);
             }
         });
         setClipToOutline(true);
     }
 
-    @Override
-    protected void onDraw(Canvas canvas) {
-        super.onDraw(canvas);
-        canvas.drawRect(rect,paint);
-        this.setBackgroundColor(Color.GRAY);
-        this.paint.setColor(Color.WHITE);
-        this.paint.setAlpha(30);
-        this.paint.setStyle(Paint.Style.FILL);//画笔属性是空心圆
-        paint.setStrokeWidth(10);//设置画笔粗细
-               /*四个参数：
-                参数一：圆心的x坐标
-                参数二：圆心的y坐标
-                参数三：圆的半径
-                参数四：定义好的画笔
-                */
-        canvas.drawCircle(
-                (float) (104 / 2.0),
-                (float) (104 / 2.0),
-                (float) (10 / 2.0),
-                this.paint);
-    }
+//    @Override
+//    protected void onDraw(Canvas canvas) {
+//        super.onDraw(canvas);
+////        canvas.drawRect(rect,paint);
+////        this.setBackgroundColor(Color.GRAY);
+////        this.paint.setColor(Color.WHITE);
+////        this.paint.setAlpha(30);
+////        this.paint.setStyle(Paint.Style.FILL);//画笔属性是空心圆
+////        paint.setStrokeWidth(10);//设置画笔粗细
+////               /*四个参数：
+////                参数一：圆心的x坐标
+////                参数二：圆心的y坐标
+////                参数三：圆的半径
+////                参数四：定义好的画笔
+////                */
+//        canvas.drawCircle(
+//                (float) (104 / 2.0),
+//                (float) (104 / 2.0),
+//                (float) (10 / 2.0),
+//                this.paint);
+//    }
 
     public void setAdjustment(float r, float g, float b, float a) {
         this.setBackgroundColor(Color.argb(a, r, g, b));
