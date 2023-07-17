fa97bc003bb5e86ae929c2f7a941198906959b6b
pigerzhu <64568939+pigerzhu@users.noreply.github.com>
2023-07-17 16:37:00 +0800

    Update AdjustmentView.java

--- app/src/main/java/minun/zte/axon30/under_screen_adjustment/AdjustmentView.java
+++ app/src/main/java/minun/zte/axon30/under_screen_adjustment/AdjustmentView.java
@@ -3,36 +3,31 @@ package minun.zte.axon30.under_screen_adjustment;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.Outline;
 import android.graphics.Paint;
+import android.graphics.Rect;
+import android.util.Log;
 import android.view.View;
+import android.view.ViewOutlineProvider;
 
 public class AdjustmentView extends View {
-
-    private Paint paint;
 
     public AdjustmentView(Context context) {
         super(context);
         this.setBackgroundColor(Color.TRANSPARENT);
-
-        paint = new Paint();
-        paint.setStyle(Paint.Style.FILL);
-    }
-
-    @Override
-    protected void onDraw(Canvas canvas) {
-        super.onDraw(canvas);
-        
-        int centerX = 100;
-        int centerY = 100;
-        int radius = 50;
-        
-        paint.setColor(getBackgroundColor());
-        canvas.drawCircle(centerX, centerY, radius, paint);
+        setOutlineProvider(new ViewOutlineProvider() {
+            @Override
+            public void getOutline(View view, Outline outline) {
+                Rect rect = new Rect(8,10,92,94);
+                Log.d("AdjustmentView", "rect: "+ rect);
+                outline.setRoundRect(rect, 45);
+            }
+        });
+        setClipToOutline(true);
     }
 
     public void setAdjustment(float r, float g, float b, float a) {
         this.setBackgroundColor(Color.argb(a, r, g, b));
-        invalidate();
     }
 
 }
