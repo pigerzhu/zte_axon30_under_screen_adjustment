97f9905cad7f5c2644a06020908e530c793225c9
pigerzhu <64568939+pigerzhu@users.noreply.github.com>
2023-07-17 16:35:05 +0800

    Update AdjustmentView.java

--- app/src/main/java/minun/zte/axon30/under_screen_adjustment/AdjustmentView.java
+++ app/src/main/java/minun/zte/axon30/under_screen_adjustment/AdjustmentView.java
@@ -4,42 +4,35 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
-import android.util.Log;
 import android.view.View;
 
 public class AdjustmentView extends View {
 
-    private float centerX, centerY, radius;
     private Paint paint;
 
     public AdjustmentView(Context context) {
         super(context);
         this.setBackgroundColor(Color.TRANSPARENT);
-        paint = new Paint();
-    }
 
-    @Override
-    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
-        super.onSizeChanged(w, h, oldw, oldh);
-        centerX = 50; // 圆心 x 坐标为 50 像素
-        centerY = 50; // 圆心 y 坐标为 50 像素
-        radius = 20; // 半径为 20 像素
+        paint = new Paint();
+        paint.setStyle(Paint.Style.FILL);
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        // 绘制圆形背景
-        paint.setColor(Color.WHITE);
+        
+        int centerX = 100;
+        int centerY = 100;
+        int radius = 50;
+        
+        paint.setColor(getBackgroundColor());
         canvas.drawCircle(centerX, centerY, radius, paint);
-        // 绘制调整效果
-        paint.setColor(Color.RED);
-        canvas.drawCircle(centerX, centerY, radius * 0.8f, paint);
     }
 
     public void setAdjustment(float r, float g, float b, float a) {
-        paint.setColor(Color.argb(a, r, g, b));
-        invalidate(); // 重绘视图
+        this.setBackgroundColor(Color.argb(a, r, g, b));
+        invalidate();
     }
 
 }
