601472faefc5b46ce24aaac4b1bf428ddf1f16a7
pigerzhu <64568939+pigerzhu@users.noreply.github.com>
2023-07-17 16:26:04 +0800

    Update AdjustmentView.java

--- app/src/main/java/minun/zte/axon30/under_screen_adjustment/AdjustmentView.java
+++ app/src/main/java/minun/zte/axon30/under_screen_adjustment/AdjustmentView.java
@@ -1,63 +1,45 @@
 package minun.zte.axon30.under_screen_adjustment;
 
 import android.content.Context;
-
 import android.graphics.Canvas;
 import android.graphics.Color;
-
-import android.graphics.Outline;
 import android.graphics.Paint;
-import android.graphics.Rect;
 import android.util.Log;
 import android.view.View;
-import android.view.ViewOutlineProvider;
 
 public class AdjustmentView extends View {
 
-//    Paint paint;
+    private float centerX, centerY, radius;
+    private Paint paint;
 
     public AdjustmentView(Context context) {
         super(context);
         this.setBackgroundColor(Color.TRANSPARENT);
-//        this.paint = new Paint();
-//        this.paint.setAlpha(100);
-        setOutlineProvider(new ViewOutlineProvider() {
-            @Override
-            public void getOutline(View view, Outline outline) {
-                Rect rect = new Rect(8,10,92,94);
-                Log.d("AdjustmentView", "rect: "+ rect);
-                outline.setRoundRect(rect, 45);
-            }
-        });
-        setClipToOutline(true);
+        paint = new Paint();
     }
 
-//    @Override
-//    protected void onDraw(Canvas canvas) {
-//        super.onDraw(canvas);
-////        canvas.drawRect(rect,paint);
-////        this.setBackgroundColor(Color.GRAY);
-////        this.paint.setColor(Color.WHITE);
-////        this.paint.setAlpha(30);
-////        this.paint.setStyle(Paint.Style.FILL);//画笔属性是空心圆
-////        paint.setStrokeWidth(10);//设置画笔粗细
-////               /*四个参数：
-////                参数一：圆心的x坐标
-////                参数二：圆心的y坐标
-////                参数三：圆的半径
-////                参数四：定义好的画笔
-////                */
-//        canvas.drawCircle(
-//                (float) (104 / 2.0),
-//                (float) (104 / 2.0),
-//                (float) (10 / 2.0),
-//                this.paint);
-//    }
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        centerX = 50; // 圆心 x 坐标为 50 像素
+        centerY = 50; // 圆心 y 坐标为 50 像素
+        radius = 20; // 半径为 20 像素
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        // 绘制圆形背景
+        paint.setColor(Color.WHITE);
+        canvas.drawCircle(centerX, centerY, radius, paint);
+        // 绘制调整效果
+        paint.setColor(Color.RED);
+        canvas.drawCircle(centerX, centerY, radius * 0.8f, paint);
+    }
 
     public void setAdjustment(float r, float g, float b, float a) {
-        this.setBackgroundColor(Color.argb(a, r, g, b));
-//        this.paint.setColor(Color.argb(a, r, g, b));
-//        this.invalidate();
+        paint.setColor(Color.argb(a, r, g, b));
+        invalidate(); // 重绘视图
     }
 
 }
